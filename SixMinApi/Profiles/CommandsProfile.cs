6f3d8d7f9c16bdb428227fbb053db917cfc2762f
JP <joaobianchi@live.com>
2023-12-29 18:29:38 -0300

    :construction: resolving branches & pushing code

---
+++ SixMinApi/Profiles/CommandsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SixMinApi.Dtos;
+using SixMinApi.Models;
+
+namespace SixMinApi.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<CommandUpdateDto, Command>();
+        }
+    }
+}
